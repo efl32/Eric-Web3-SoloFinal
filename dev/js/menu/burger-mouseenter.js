d69d3091522af887554b25600174986e4113ba46
Eric Lauterbach <efl32@drexel.edu>
2020-03-03 11:37:50 -0500

    burger tweaks

--- dev/js/menu/burger-mouseenter.js
+++ dev/js/menu/burger-mouseenter.js
@@ -1,13 +1,13 @@
 burgerHover = gsap.timeline({paused: true});
 
-burgerHover.to("#arrow1", {duration: 0.15, alpha:1, y: -10})
-.to("#arrow2", {duration: 0.15, alpha:1, y: -10})
+burgerHover.to("#burger", {duration: 0.25, scaleX: .2},"click")
+.to("#topLine", {duration: 0.25, y: 50},"click")
+.to("#bottomLine", {duration: 0.25, y: -50},"click");
 
 $("#burgerContainer").on("mouseenter", function(){
     console.log("mouse enter");
 
     if(canYouSeeNav === false){
-        burgerHover.invalidate().restart();
-        burgerHover.play().repeat(-1).repeatDelay(0.2);
+        burgerHover.play();
     }
 });
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,3 +2,5 @@
 // @codekit-prepend "menu/burger-mouseenter.js";
 // @codekit-prepend "menu/burger-mouseleave.js";
 // @codekit-prepend "menu/burger-click.js";
+
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
