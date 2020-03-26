60d7f8bcdab273bba3d86355efcf4db4e8386b71
Eric Lauterbach <efl32@drexel.edu>
2020-03-25 23:01:32 -0700

    mobile layout + added address info

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,14 +2,14 @@
 
 gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
 
-gsap.to("#svg1a", {duration:3, morphSVG: "#svg1b"});
-gsap.to("#svg2a", {duration:3, morphSVG: "#svg2b"});
-gsap.to("#svg3a", {duration:3, morphSVG: "#svg3b"});
-gsap.to("#svg4a", {duration:3, morphSVG: "#svg4b"});
-gsap.to("#svg5a", {duration:3, morphSVG: "#svg5b"});
-gsap.to("#svg6a", {duration:3, morphSVG: "#svg6b"});
-gsap.to("#svg7a", {duration:3, morphSVG: "#svg7b"});
-gsap.to("#svg8a", {duration:3, morphSVG: "#svg8b"});
-gsap.to("#svg9a", {duration:3, morphSVG: "#svg9b"});
-gsap.to("#svg10a", {duration:3, morphSVG: "#svg10b"});
-gsap.to("#blankBGa", {duration:3, morphSVG: "#blankBGb"});
+gsap.to("#svg1a", {duration:1, morphSVG: "#svg1b"});
+gsap.to("#svg2a", {duration:1, morphSVG: "#svg2b"});
+gsap.to("#svg3a", {duration:1, morphSVG: "#svg3b"});
+gsap.to("#svg4a", {duration:1, morphSVG: "#svg4b"});
+gsap.to("#svg5a", {duration:1, morphSVG: "#svg5b"});
+gsap.to("#svg6a", {duration:1, morphSVG: "#svg6b"});
+gsap.to("#svg7a", {duration:1, morphSVG: "#svg7b"});
+gsap.to("#svg8a", {duration:1, morphSVG: "#svg8b"});
+gsap.to("#svg9a", {duration:1, morphSVG: "#svg9b"});
+gsap.to("#svg10a", {duration:1, morphSVG: "#svg10b"});
+gsap.to("#blankBGa", {duration:1, morphSVG: "#blankBGb"});
